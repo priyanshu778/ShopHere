cd5eb7efe219ce05eef33753702b4c59b1e479f8
priyanshu778 <priyanshuk7788@gmail.com>
2025-04-16 19:34:53 +0530

    Update MyState.jsx

--- src/context/MyState.jsx
+++ src/context/MyState.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
-import MyContext from './myContext';
+import MyContext from './MyContext';
 import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from 'firebase/firestore';
 import { fireDB } from '../Firebase/Firebase';
 import toast from 'react-hot-toast';
